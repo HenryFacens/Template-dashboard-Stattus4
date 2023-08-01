8e09072a90b3db7737fde4bc81cde064379d4a51
Henry Santuriao <henry.almeida@stattus4.com>
2023-08-01 10:37:56 -0300

    feat: conection DB

--- core/settings.py
+++ core/settings.py
@@ -71,8 +71,12 @@ WSGI_APPLICATION = 'core.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'LsiNgRIlcCDn',
+        'HOST': 'prod-4fluid-iot-aurora-cluster.cluster-ro-c6zjoohnrdv4.sa-east-1.rds.amazonaws.com',
+        'PORT': '5432',
     }
 }
 
