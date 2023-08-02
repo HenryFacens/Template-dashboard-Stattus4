b30af0ae764b43d3d00a8433fbab90ee4018fc42
Henry Santuriao <henry.almeida@stattus4.com>
2023-08-02 10:46:18 -0300

    fix:change / on urls.py

--- apps/home/urls.py
+++ apps/home/urls.py
@@ -5,11 +5,11 @@ from apps.boletim.views import Boletim_fluid
 
 urlpatterns = [
     # Dashboards
-    path("/dash-fluid", Fluid.as_view(), name="dash-fluid"),
-    path("/dash-ada", Ada.as_view(), name="dash-ada"),
+    path("dash-fluid/", Fluid.as_view(), name="dash-fluid"),
+    path("dash-ada/", Ada.as_view(), name="dash-ada"),
 
     # Boletins
-    path("/boletim-fluid", Boletim_fluid.as_view(), name="boletim-fluid"),
+    path("boletim-fluid/", Boletim_fluid.as_view(), name="boletim-fluid"),
 
     path('', views.index, name='home'),
     re_path(r'^.*\.*', views.pages, name='pages'),
