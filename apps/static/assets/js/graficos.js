818645886f48113fc3bfbbc666ecc88474fe4650
Henry Santuriao <henry.almeida@stattus4.com>
2023-08-11 09:27:29 -0300

    feat:grafico de quantidades do Boletim

--- apps/static/assets/js/graficos.js
+++ apps/static/assets/js/graficos.js
@@ -1,3 +1,21 @@
+Chart.plugins.register({
+    afterDraw: function(chart) {
+        var ctx = chart.ctx;
+
+        ctx.font = Chart.helpers.fontString(Chart.defaults.global.defaultFontSize, Chart.defaults.global.defaultFontStyle, Chart.defaults.global.defaultFontFamily);
+        ctx.textAlign = 'center';
+        ctx.textBaseline = 'bottom';
+
+        chart.data.datasets.forEach(function(dataset, i) {
+            var meta = chart.getDatasetMeta(i);
+            meta.data.forEach(function(bar, index) {
+                var data = dataset.data[index];
+                ctx.fillText(data, bar._model.x, bar._model.y - 5);
+            });
+        });
+    }
+});
+
 function initChart($chartElement, labels, data) {
     var ordersChart = new Chart($chartElement, {
         type: 'bar',
@@ -6,8 +24,8 @@ function initChart($chartElement, labels, data) {
             datasets: [{
                 label: 'Sales',
                 data: data,
-                backgroundColor: 'rgba(75, 192, 192, 0.2)',  // Pode ajustar conforme sua preferência
-                borderColor: 'rgba(75, 192, 192, 1)',  // Pode ajustar conforme sua preferência
+                backgroundColor: 'rgba(75, 192, 192, 0.2)', 
+                borderColor: 'rgba(75, 192, 192, 1)',  
                 borderWidth: 1
             }]
         },
@@ -17,28 +35,8 @@ function initChart($chartElement, labels, data) {
                     beginAtZero: true
                 }
             },
-            plugins: {
-                tooltip: {
-                    enabled: false
-                }
-            },
-            animation: {
-                onComplete: function () {
-                    var chartInstance = this.chart,
-                        ctx = chartInstance.ctx;
-
-                    ctx.font = Chart.helpers.fontString(Chart.defaults.global.defaultFontSize, Chart.defaults.global.defaultFontStyle, Chart.defaults.global.defaultFontFamily);
-                    ctx.textAlign = 'center';
-                    ctx.textBaseline = 'bottom';
-
-                    this.data.datasets.forEach(function (dataset, i) {
-                        var meta = chartInstance.controller.getDatasetMeta(i);
-                        meta.data.forEach(function (bar, index) {
-                            var data = dataset.data[index];
-                            ctx.fillText(data, bar._model.x, bar._model.y - 5);
-                        });
-                    });
-                }
+            tooltips: {
+                enabled: false
             }
         }
     });
