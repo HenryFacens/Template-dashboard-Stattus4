7adaa1a5f7171509038063645c27af031aab2451
Henry Santuriao <henry.almeida@stattus4.com>
2023-08-14 14:01:52 -0300

    feat:Adicionando o icone de lampada

--- apps/home/templatetags/custom_filters.py
+++ apps/home/templatetags/custom_filters.py
@@ -7,3 +7,22 @@ def get_item(dictionary, key):
     if isinstance(dictionary, dict):
         return dictionary.get(key, "-")
     return "-"
+
+MONTHS_ORDER = {
+    'Janeiro': 1,
+    'Fevereiro': 2,
+    'Março': 3,
+    'Abril': 4,
+    'Maio': 5,
+    'Junho': 6,
+    'Julho': 7,
+    'Agosto': 8,
+    'Setembro': 9,
+    'Outubro': 10,
+    'Novembro': 11,
+    'Dezembro': 12,
+}
+
+@register.filter
+def sort_months(month_list):
+    return sorted(month_list, key=lambda month: MONTHS_ORDER.get(month, 0))
